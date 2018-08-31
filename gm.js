2438a06c7d66aff94b077987da3dd33d1a545f6b
Guimao Dai <guimaodai@gmail.com>
2018-08-31 18:03:43 +0800

    better build

--- gm.js
+++ gm.js
@@ -8,15 +8,18 @@
 let argv = require("argp").createParser({
 		once: true
 	})
+	.on('end', function(argv) {
+		if (!argv.build) this.printHelp();
+	})
 	.description("Gm help.")
 	.email("wyyxdgm@163.com")
 	.body()
-	//The object and argument definitions and the text of the --help message
-	//are configured at the same time
-	// .text(" Arguments:")
-	// .argument("path", {
-	// 	description: "Path of the target file that will be bulilded, default value is defined in gm.json by the 'main' property"
-	// })
+	// The object and argument definitions and the text of the --help message
+	// are configured at the same time
+	.text(" Arguments:")
+	.argument("build", {
+		description: "compile && build file"
+	})
 	.text("\n Options:")
 	.option({
 		short: "d",
@@ -46,7 +49,8 @@ let argv = require("argp").createParser({
 	.version("v1.0.0")
 	.argv();
 
-let util = require('./lib/util.js');
-util.init(argv);
-
-util.build(argv.directory ? require('path').resolve(process.cwd(), argv.directory) : process.cwd(), argv['output'], argv['append-array']);
+if (argv.build) {
+	let util = require('./lib/util.js');
+	util.init(argv);
+	util.build(argv.directory ? require('path').resolve(process.cwd(), argv.directory) : process.cwd(), argv['output'], argv['append-array']);
+}
